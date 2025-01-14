bfbf331ed4bec3f6958d13be69a7e5e400b37cc8
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-14 08:15:39 -0500

    Create Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RamirezforaneoApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Category Name is required")]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
