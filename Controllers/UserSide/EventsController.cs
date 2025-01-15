3f9e6c97c616ad0b25d79adb95454ff806944a50
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-15 16:11:59 -0500

    Add User Events side view

---
+++ Controllers/UserSide/EventsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RamirezforaneoApp.Data;
+using RamirezforaneoApp.Models;
+
+namespace RamirezforaneoApp.Controllers.UserSide
+{
+    public class EventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EventsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Event.Include(m => m.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Event
+                .Include(m => m.Category)
+                .FirstOrDefaultAsync(m => m.EventId == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+
+        // GET: Events/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        // POST: Events/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventId,EventTitle,EventDescription,CategoryId,EventLocation,EventStartDate,EventEndDate,EventCreationDate")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", @event.CategoryId);
+            return View(@event);
+        }
+
+
+
+
+    }
+}
