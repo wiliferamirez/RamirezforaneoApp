e5b18c07e5624a3a2faa310ff9831658868f7bdd
William Fernando Ramirez Paucar <william.ramirez.paucar@udla.edu.ec>
2025-01-15 17:24:27 -0500

    Create Market Model

---
+++ Models/Market.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RamirezforaneoApp.Models
+{
+    public class Market
+    {
+        [Key]
+        public int MarketItemId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [ValidateNever]
+        public string ItemSeller { get; set; }
+
+        [Required(ErrorMessage = "Please enter the name of the Item")]
+        [StringLength(100, ErrorMessage = "Item Name cannot exceed 100 characters")]
+        [Display(Name = "Item Name")]
+        public string ItemName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the description of the item")]
+        [StringLength(600, ErrorMessage = "Item description cannot exceed 600 characters")]
+        [Display(Name = "Item Description")]
+        public string ItemDescription { get; set; }
+
+        [Url(ErrorMessage = "Invalid URL format")]
+        [Display(Name = "Image URL")]
+        public string ItemImageUrl { get; set; }
+
+        [Required(ErrorMessage = "Please enter the price of the item")]
+        [DataType(DataType.Currency)]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal ItemPrice { get; set; }
+
+        [Required(ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [ValidateNever]
+        public Category Category { get; set; }
+
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Creation Date")]
+        [ValidateNever]
+        public DateTime ItemCreationDate { get; set; } = DateTime.Now;
+
+    }
+}
